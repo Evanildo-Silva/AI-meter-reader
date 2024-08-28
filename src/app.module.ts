cefe32f57c9524f80d0e5806e02a4e01cdd70b95
Evanildo Silva <evanildo.sf91@gmail.com>
2024-08-28 17:04:26 -0300

    refactor: add typeorm db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReaderModule } from './reader/reader.module';
@@ -7,7 +8,16 @@ import { ReaderModule } from './reader/reader.module';
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env.development.local'],
-  }), ReaderModule],
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USERNAME
+  }),
+    ReaderModule],
   controllers: [AppController],
   providers: [AppService],
 })
