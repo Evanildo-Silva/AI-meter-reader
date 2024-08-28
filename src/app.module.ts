5bc6699720c89ef191344aee6370dbb2cf919491
Evanildo Silva <evanildo.sf91@gmail.com>
2024-08-28 16:12:37 -0300

    feat: create reader resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReaderModule } from './reader/reader.module';
 
 @Module({
-  imports: [],
+  imports: [ReaderModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
