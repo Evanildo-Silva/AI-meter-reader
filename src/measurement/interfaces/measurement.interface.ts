b456b8d02bf71de6295bc1bcca4f5d0b474a16a8
Evanildo Silva <evanildo.sf91@gmail.com>
2024-08-30 22:53:06 -0300

    refactor: replace interface IMeasure

--- src/measurement/interfaces/measurement.interface.ts
+++ src/measurement/interfaces/measurement.interface.ts
@@ -2,7 +2,7 @@ export interface IMeasurement {
     measure_uuid: string;
     measure_datetime: Date;
     measure_type: 'WATER' | 'GAS';
-    image: string;
+    image_url: string;
     customer_code: string;
     measure_value: number;
     has_confirmed: boolean;
